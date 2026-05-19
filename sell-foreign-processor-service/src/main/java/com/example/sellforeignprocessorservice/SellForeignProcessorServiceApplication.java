fc9f1fa1aecdb3b2ad83e82a0705666c9dd31d87
linh-ca-nhan <hailinh2910203@gmail.com>
2026-05-19 17:00:18 +0700

    Add Logstash logging configuration and dependencies for all services

--- sell-foreign-processor-service/src/main/java/com/example/sellforeignprocessorservice/SellForeignProcessorServiceApplication.java
+++ sell-foreign-processor-service/src/main/java/com/example/sellforeignprocessorservice/SellForeignProcessorServiceApplication.java
@@ -1,15 +1,19 @@
 package com.example.sellforeignprocessorservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@Slf4j
 public class SellForeignProcessorServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellForeignProcessorServiceApplication.class, args);
+        log.info("Application [SELL-FOREIGN-PROCESSOR-SERVICE] started successfully");
+
     }
 
 }
--- treasury-service/src/main/java/com/example/treasuryservice/TreasuryServiceApplication.java
+++ treasury-service/src/main/java/com/example/treasuryservice/TreasuryServiceApplication.java
@@ -1,13 +1,15 @@
 package com.example.treasuryservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class TreasuryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TreasuryServiceApplication.class, args);
+        log.info("Application [TREASURY-SERVICE] started successfully");
     }
 
 }
